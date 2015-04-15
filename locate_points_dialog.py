9e9cf7f4bd19280686867f1078a9bdc381609788
DamnBack <damnback333@gmail.com>
2015-04-15 08:07:10 +0200

    Update locate_points_dialog.py

--- locate_points_dialog.py
+++ locate_points_dialog.py
@@ -3,7 +3,7 @@
 /***************************************************************************
  LocatePointsDialog
                                  A QGIS plugin
- Creating points along lines at given interval
+ Creating points along lines with given offset and interval
                              -------------------
         begin                : 2015-03-18
         git sha              : $Format:%H$
