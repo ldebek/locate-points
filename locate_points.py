3d635df73f117fd3ae14dee0a48b484d98f4635c
DamnBack <damnback333@gmail.com>
2015-04-15 08:04:14 +0200

    Update locate_points.py

--- locate_points.py
+++ locate_points.py
@@ -3,7 +3,7 @@
 /***************************************************************************
  LocatePoints
                                  A QGIS plugin
- Creating points along lines with given offset and interval
+ Creating points along lines with given offset and interva
                               -------------------
         begin                : 2015-03-18
         git sha              : $Format:%H$
